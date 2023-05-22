5c784327a7ece8ca797dda3159d6fcb80f55d540
FrancoAHM <folliies@gmail.com>
2023-05-22 14:03:14 -0300

    Task11.1 Terminada.

--- src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
+++ src/test/java/com/mindhub/homebanking/HomebankingApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HomebankingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
